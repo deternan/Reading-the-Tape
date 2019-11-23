69b6db3de023cda5dbdb537b6c02805720c8c8de
Chao-Hsuan Ke <chke@iir.csie.ncku.edu.tw>
2019-11-23 23:35:20 +0800

    Application GUI

---
+++ src/JFrame/Application.java
@@ -0,0 +1,53 @@
+package JFrame;
+
+/*
+ * Application GUI
+ * version: November 23, 2019 11:34 PM
+ * Last revision: November 23, 2019 11:34 PM
+ * 
+ * Author : Chao-Hsuan Ke
+ * E-mail : phelpske.dev at gmail dot com
+ * 
+ */
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Application extends JFrame 
+{
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Application frame = new Application();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Application() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
